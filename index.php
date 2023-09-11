340e01d98bdc18d7dfc1f93f2d49dd4e0823cae8
Kirill Vnukov <kirillgrandson@gmail.com>
2023-09-11 19:01:02 +0300

    Add a html structure

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Задание 1 - Интроверт</title>
+</head>
+<body>
 <?php
+
 require_once(__DIR__ . '/vendor/autoload.php');
 require_once('Solution.php');
 
@@ -14,3 +25,7 @@ if (isset($_GET['date_from']) && isset($_GET['date_to'])) {
 } else {
     echo "Введите даты для генерации отчета.";
 }
+
+?>
+</body>
+</html>
